976ed76e1763575c06c1cf5a0f18cfe871c3dcd7
Will Ransohoff <wransohoff@gmail.com>
2018-02-04 19:00:37 -0800

    Add second/millisecond/microsecond delay units

--- modules/fsm_nodes.lua
+++ modules/fsm_nodes.lua
@@ -339,10 +339,23 @@ end
 function FSMNodes.append_delay_node(node, node_graph, proj_state)
   local node_text = '  // ("Delay" node)\n'
   node_text = node_text .. '  NODE_' .. node.node_ind .. ':\n'
-  -- Get the number of cycles to delay. TODO: Support other 'delay_units'.
+  -- Get the number of cycles to delay.
   node_text = node_text .. '  delay_cycles('
   if node.options and node.options.delay_value then
-    node_text = node_text .. node.options.delay_value .. ');\n'
+    -- TODO: Support differend clock frequencies. For now, assume 48MHz.
+    local delay_scale = 1
+    if node.options.delay_units == 'us' then
+      -- 48,000,000 cycles per second = 48 cycles per microsecond.
+      delay_scale = 48
+    elseif node.options.delay_units == 'ms' then
+      -- 48,000,000 cycles per second = 48,000 cycles per millisecond.
+      delay_scale = 48000
+    elseif node.options.delay_units == 's' then
+      -- 48,000,000 cycles per second.
+      delay_scale = 48000000
+    end
+    local delay_cyc = tonumber(node.options.delay_value) * delay_scale
+    node_text = node_text .. delay_cyc .. ');\n'
   else
     node_text = node_text .. '0); // TODO: Default options\n'
   end
