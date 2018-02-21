7a7040aa3f158de643315c60d2e95f1263f8c7d6
Will Ransohoff <wransohoff@gmail.com>
2018-02-20 22:20:19 -0800

    Maybe node preprocessors would go better in module files; add an example file.

---
+++ modules/nodes/ex.lua
@@ -0,0 +1,13 @@
+local node_reqs = {}
+
+-- ('' Node)
+function node_reqs.ensure_support_methods(node, proj_state)
+  return false
+end
+
+-- ('' Node)
+function node_reqs.append_node(node, node_graph, proj_state)
+  return false
+end
+
+return node_reqs
