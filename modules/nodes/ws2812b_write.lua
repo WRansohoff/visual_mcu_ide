0afe06e73e11165c9e76070be77bfe70f67a8aba
Will Ransohoff <wransohoff@gmail.com>
2018-03-03 14:56:39 -0800

    Add 'delay' requirements to the WS2812B nodes.

--- modules/nodes/ws2812b_write.lua
+++ modules/nodes/ws2812b_write.lua
@@ -4,6 +4,33 @@ local node_reqs = {}
 
 -- ('Write Color to NeoPixel' Node)
 function node_reqs.ensure_support_methods(node, proj_state)
+  -- This node requires methods from the 'Delay' node.
+  local delay_util_s_insert_path = 'static/node_code/delay/src/util_S.insert'
+  local delay_global_h_insert_path = 'static/node_code/delay/src/global_h.insert'
+  -- 'util.S' delay declares.
+  if not varm_util.copy_block_into_file(delay_util_s_insert_path,
+                                        proj_state.base_dir .. 'src/util.S',
+                                        'UTIL_S_DELAYS_DEC_START:',
+                                        'UTIL_S_DELAYS_DEC_DONE:',
+                                        '/ ASM_GLOBAL_UTIL_DECLARES:') then
+    return nil
+  end
+  -- 'util.S' delay defines.
+  if not varm_util.copy_block_into_file(delay_util_s_insert_path,
+                                        proj_state.base_dir .. 'src/util.S',
+                                        'UTIL_S_DELAYS_DEF_START:',
+                                        'UTIL_S_DELAYS_DEF_DONE:',
+                                        '/ ASM_GLOBAL_UTIL_DEFINES:') then
+    return nil
+  end
+  -- 'global.h' delay declares.
+  if not varm_util.copy_block_into_file(delay_global_h_insert_path,
+                                        proj_state.base_dir .. 'src/global.h',
+                                        'GLOBAL_EXTERN_DELAYS_START:',
+                                        'GLOBAL_EXTERN_DELAYS_DONE:',
+                                        '/ ASM_METHOD_DEFINES:') then
+    return nil
+  end
   return true
 end
 
